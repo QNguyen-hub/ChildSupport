ef64472d651525063b22d510fcde81082b9bdc26
Q <qnguyen303@gmail.com>
2023-01-02 00:53:09 -0700

    added export for components for Intake

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Order from "./intake/Order";
+
+const Main = () => {
+  return (
+    <div>
+      <Order />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/intake/Order.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Order = () => {
+  return (
+    <div>
+      <h1>Is there a Child Support Order?</h1>
+      <h2>Yes</h2>
+      <h2>No</h2>
+    </div>
+  );
+};
+
+export default Order;
